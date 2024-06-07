a762dfbc2e50d19a0119cfc9c1199bb784ab26b0
Reezan <100752303+ReeAya@users.noreply.github.com>
2024-06-07 15:22:18 +0900

    Initial upload of completed project

---
+++ script.js
@@ -0,0 +1,34 @@
+import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
+
+createApp({
+  data() {
+    return {
+      tasks: JSON.parse(localStorage.getItem("tasks")),
+      enteredValue: "",
+    };
+  },
+  computed: {
+    headText() {
+      return this.tasks.length === 0 ? "No tasks for today!" : "Today's tasks";
+    },
+  },
+  watch: {
+    tasks() {
+      this.tasks = localStorage.getItem("tasks");
+    },
+  },
+  methods: {
+    addTask() {
+      if (!this.enteredValue) return;
+      this.tasks.push(this.enteredValue);
+
+      localStorage.setItem("tasks", JSON.stringify(this.tasks));
+      this.enteredValue = "";
+    },
+
+    removeTask(idx) {
+      this.tasks.splice(idx, 1);
+      localStorage.setItem("tasks", JSON.stringify(this.tasks));
+    },
+  },
+}).mount("#planner");
